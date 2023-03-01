b89a0ad24abae719e8838d0343f25360e919e79e
Dan Addington <kiwiaddo@gmail.com>
2023-03-02 11:49:47 +1300

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,9 @@
+network_name = "abc123"
+project_id = var.project_id
+subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
