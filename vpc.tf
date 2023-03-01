e2302d45839a09c646875d4a7c0f8c49fc266354
Dan Addington <kiwiaddo@gmail.com>
2023-03-02 11:52:56 +1300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,16 @@
-network_name = "abc123"
-project_id = var.project_id
-subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 6.0"
+
+    project_id   = var.project_id
+    network_name = "example-vpc"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+      {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+      }
+    ]
+}
